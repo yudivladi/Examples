0f77e042d12468ddbb42313378652bc9746031c5
yudivladi <yudi.vladislav@gmail.com>
2024-05-16 19:36:04 +0200

    ##

---
+++ src/App.vue
@@ -0,0 +1,190 @@
+<script setup></script>
+
+<template>
+  <div class="w-4/5 m-auto h-screen rounded-xl shadow-xl bg-black">
+    <header class="border-b border-slate-300 px-12 py-2">
+      <ul class="flex justify-between items-center">
+        <button></button>
+        <img src="./assets/icons/s@.png" alt="logo" class="w-48" />
+        <button class="bg-yellow-500 hover:bg-yellow-700 text-black font-bold py-1 px-5">
+          TICKETS
+        </button>
+      </ul>
+    </header>
+
+    <section class="flex overflow-x-auto mt-32">
+      <div class="flex text-center space-x-6 snap-x">
+        <div
+          class="p-6 w-[330px] h-[518px] span-center bg-black-rgba rounded-3xl bg-rose-500 shadow-2xl shadow-red-900 text-center items-center justify-center"
+        >
+          <div class="text-3xl font-bold mt-4 mb-8 text-white">Basic</div>
+          <ul class="text-lg space-y-6">
+            <li>diam maecenas ultricies mi</li>
+            <li>semper risus in hendrerit</li>
+            <li>litricies integer quis</li>
+            <li>vitae auctor eu augue</li>
+            <li>faucibus interdum posuere lorem</li>
+          </ul>
+          <div class="text-lg font-serif mt-14">
+            <button
+              class="rounded-2xl mb-4 bg-yellow-500 hover:bg-yellow-700 text-black font-bold py-1 px-5"
+            >
+              MORE INFORMATION
+            </button>
+            <button
+              class="rounded-2xl mb-4 bg-yellow-500 hover:bg-black text-black hover:text-white font-bold py-1 px-5"
+            >
+              BUY YOUR TICKET
+            </button>
+          </div>
+        </div>
+        <div
+          class="p-6 w-[330px] h-[518px] span-center bg-black-rgba rounded-3xl bg-rose-500 shadow-2xl shadow-red-900 text-center items-center justify-center"
+        >
+          <div class="text-3xl font-bold mt-4 mb-8 text-white">Basic</div>
+          <ul class="text-lg space-y-6">
+            <li>diam maecenas ultricies mi</li>
+            <li>semper risus in hendrerit</li>
+            <li>litricies integer quis</li>
+            <li>vitae auctor eu augue</li>
+            <li>faucibus interdum posuere lorem</li>
+          </ul>
+          <div class="text-lg font-serif mt-14">
+            <button
+              class="rounded-2xl mb-4 bg-yellow-500 hover:bg-yellow-700 text-black font-bold py-1 px-5"
+            >
+              MORE INFORMATION
+            </button>
+            <button
+              class="rounded-2xl mb-4 bg-yellow-500 hover:bg-black text-black hover:text-white font-bold py-1 px-5"
+            >
+              BUY YOUR TICKET
+            </button>
+          </div>
+        </div>
+        <div
+          class="p-6 w-[330px] h-[518px] span-center bg-black-rgba rounded-3xl bg-rose-500 shadow-2xl shadow-red-900 text-center items-center justify-center"
+        >
+          <div class="text-3xl font-bold mt-4 mb-8 text-white">Basic</div>
+          <ul class="text-lg space-y-6">
+            <li>diam maecenas ultricies mi</li>
+            <li>semper risus in hendrerit</li>
+            <li>litricies integer quis</li>
+            <li>vitae auctor eu augue</li>
+            <li>faucibus interdum posuere lorem</li>
+          </ul>
+          <div class="text-lg font-serif mt-14">
+            <button
+              class="rounded-2xl mb-4 bg-yellow-500 hover:bg-yellow-700 text-black font-bold py-1 px-5"
+            >
+              MORE INFORMATION
+            </button>
+            <button
+              class="rounded-2xl mb-4 bg-yellow-500 hover:bg-black text-black hover:text-white font-bold py-1 px-5"
+            >
+              BUY YOUR TICKET
+            </button>
+          </div>
+        </div>
+        <div
+          class="p-6 w-[330px] h-[518px] span-center bg-black-rgba rounded-3xl bg-rose-500 shadow-2xl shadow-red-900 text-center items-center justify-center"
+        >
+          <div class="text-3xl font-bold mt-4 mb-8 text-white">Basic</div>
+          <ul class="text-lg space-y-6">
+            <li>diam maecenas ultricies mi</li>
+            <li>semper risus in hendrerit</li>
+            <li>litricies integer quis</li>
+            <li>vitae auctor eu augue</li>
+            <li>faucibus interdum posuere lorem</li>
+          </ul>
+          <div class="text-lg font-serif mt-14">
+            <button
+              class="rounded-2xl mb-4 bg-yellow-500 hover:bg-yellow-700 text-black font-bold py-1 px-5"
+            >
+              MORE INFORMATION
+            </button>
+            <button
+              class="rounded-2xl mb-4 bg-yellow-500 hover:bg-black text-black hover:text-white font-bold py-1 px-5"
+            >
+              BUY YOUR TICKET
+            </button>
+          </div>
+        </div>
+        <div
+          class="p-6 w-[330px] h-[518px] span-center bg-black-rgba rounded-3xl bg-rose-500 shadow-2xl shadow-red-900 text-center items-center justify-center"
+        >
+          <div class="text-3xl font-bold mt-4 mb-8 text-white">Basic</div>
+          <ul class="text-lg space-y-6">
+            <li>diam maecenas ultricies mi</li>
+            <li>semper risus in hendrerit</li>
+            <li>litricies integer quis</li>
+            <li>vitae auctor eu augue</li>
+            <li>faucibus interdum posuere lorem</li>
+          </ul>
+          <div class="text-lg font-serif mt-14">
+            <button
+              class="rounded-2xl mb-4 bg-yellow-500 hover:bg-yellow-700 text-black font-bold py-1 px-5"
+            >
+              MORE INFORMATION
+            </button>
+            <button
+              class="rounded-2xl mb-4 bg-yellow-500 hover:bg-black text-black hover:text-white font-bold py-1 px-5"
+            >
+              BUY YOUR TICKET
+            </button>
+          </div>
+        </div>
+        <div
+          class="p-6 w-[330px] h-[518px] span-center bg-black-rgba rounded-3xl bg-rose-500 shadow-2xl shadow-red-900 text-center items-center justify-center"
+        >
+          <div class="text-3xl font-bold mt-4 mb-8 text-white">Basic</div>
+          <ul class="text-lg space-y-6">
+            <li>diam maecenas ultricies mi</li>
+            <li>semper risus in hendrerit</li>
+            <li>litricies integer quis</li>
+            <li>vitae auctor eu augue</li>
+            <li>faucibus interdum posuere lorem</li>
+          </ul>
+          <div class="text-lg font-serif mt-14">
+            <button
+              class="rounded-2xl mb-4 bg-yellow-500 hover:bg-yellow-700 text-black font-bold py-1 px-5"
+            >
+              MORE INFORMATION
+            </button>
+            <button
+              class="rounded-2xl mb-4 bg-yellow-500 hover:bg-black text-black hover:text-white font-bold py-1 px-5"
+            >
+              BUY YOUR TICKET
+            </button>
+          </div>
+        </div>
+        <div
+          class="p-6 w-[330px] h-[518px] span-center bg-black-rgba rounded-3xl bg-rose-500 shadow-2xl shadow-red-900 text-center items-center justify-center"
+        >
+          <div class="text-3xl font-bold mt-4 mb-8 text-white">Basic</div>
+          <ul class="text-lg space-y-6">
+            <li>diam maecenas ultricies mi</li>
+            <li>semper risus in hendrerit</li>
+            <li>litricies integer quis</li>
+            <li>vitae auctor eu augue</li>
+            <li>faucibus interdum posuere lorem</li>
+          </ul>
+          <div class="text-lg font-serif mt-14">
+            <button
+              class="rounded-2xl mb-4 bg-yellow-500 hover:bg-yellow-700 text-black font-bold py-1 px-5"
+            >
+              MORE INFORMATION
+            </button>
+            <button
+              class="rounded-2xl mb-4 bg-yellow-500 hover:bg-black text-black hover:text-white font-bold py-1 px-5"
+            >
+              BUY YOUR TICKET
+            </button>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<style scoped></style>
